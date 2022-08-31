f62294bccda0a8c3ef62dd625636fcdb134b214a
AngelTerziev <angelterziev195@gmail.com>
2022-08-31 23:30:45 +0300

    testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 // import logo from './logo.svg';
 import './App.scss';
+import QrCode from './qr-gen/qr-gen';
 
 function App() {
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <QrCode />
+        </div>
+    );
 }
 
 export default App;
